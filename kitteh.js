0d99950ad6ebcf7519c09a72a804508d76fe715d
Mustafa Rizvi <mustafa@Maude.local>
2013-02-08 01:28:10 -0500

    progress...

--- kitteh.js
+++ kitteh.js
@@ -1,15 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
+// Module dependencies.
+var express = require('express'),
+  stylus = require('stylus'),
+  nib = require('nib'),
+  routes = require('./routes');
 
 var app = module.exports = express();
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -18,6 +15,21 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
+
+// View stuff
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -28,8 +40,8 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
 
+// Startup
 app.listen(2345);
 console.log("Express server listening on port %d in %s mode", '2345', app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,52 @@
 
 /*
- * GET home page.
+ * GET KITTEH!!
  */
 
 exports.index = function (req, res) {
-  var $ = require('jquery')
-    , mongojs = require('mongojs')
-    , canvas = require('canvas')
-    , kittydar = require('kittydar')
-    , clientId = '2e0077af5c2b4aef81c5f4349ac1558d'
-    , dbName = 'apps'
-    , collectionName = 'kitteh'
-    , apiUrl = 'https://api.instagram.com/v1/tags/kitty/media/recent?client_id=' + clientId;
+  var $ = require('jquery'),
+    Mongojs = require('mongojs');
+  var clientId = '2e0077af5c2b4aef81c5f4349ac1558d',
+    dbName = 'apps',
+    collectionName = 'kitteh',
+    apiUrl = 'https://api.instagram.com/v1/tags/kitty/media/recent?client_id=' + clientId;
 
   // set up database
-  var db = mongojs(dbName),
-      kittehCollection = db.collection(collectionName);
+  var db = Mongojs(dbName, [collectionName]);
 
   // fetch kitteh
   $.getJSON(apiUrl, function(response){
-    for (var i=0; i<response.data.length; i++) {
-      var item = response.data[i];
-      db.kittehCollection.find({id: item.id}, function (err, docs) {
-        console.log(docs);
-      });
-      console.log(item.id);
-    }
+    var random = Math.floor(Math.random() * (response.data.length + 1)),
+        item = response.data[random];
+    kitteh = {
+          'id': item['id'],
+          'timeCreated': Number(item['created_time']),
+          'timeAdded': Math.round((new Date()).getTime() / 1000),
+          'link': item['link'],
+          'url': item['images']['standard_resolution']['url'],
+          'isConfirmed': false
+        };
+    
+    db[collectionName].find({id: kitteh.id}, function (err, docs) {
+      if (err) throw err;
+
+      // kitteh not found. add it?
+      if (docs.length === 0) {
+        db[collectionName].save(kitteh);
+
+      // seen this kitteh
+      } else {
+      }
+    });
+
+    // send to view
+    db[collectionName].find({}, function (err, docs) {
+      res.render ('index', {
+        title: 'Kitteh',
+        kitteh: docs
+      })
+    });
+
   });
-  res.render ('index', {
-    title: 'Kitteh'
-  })
+
 };
